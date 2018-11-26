4de88414b1f49b6f6dbf452521c4ebba4572834f
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-11-26 15:53:46 -0500

    added first part of struct

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Arvin Zojaji on 2018-11-26.
+//  Copyright © 2018 Arvin Zojaji. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo {
+    var title: String
+    var dueDate: Date
+    
+}
